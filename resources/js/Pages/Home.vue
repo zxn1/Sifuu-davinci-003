f26206fc25edae5decab3b77d37a6c93f9ea6f03
Muhd Jamil Husni <maintanance1991@gmail.com>
2023-04-01 18:16:14 +0800

    setup inertia + vue + laravel + tailwind

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<AppLayout></AppLayout>
+</template>
+
+<script>
+import AppLayout from "../Layout/Layout.vue";
+
+export default {
+    components: {
+        AppLayout,
+    }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
